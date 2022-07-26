831268c0ed480345bddbf78d24fc3bb91821e14d
ZhiyuWang <zcabanq@uc.ac.uk>
2022-07-26 21:05:32 +0800

    add the-footer component

---
+++ web/src/components/the-footer.vue
@@ -0,0 +1,17 @@
+<template>
+  <a-layout-footer style="text-align: center">
+      wz14123214214zwy
+    </a-layout-footer>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'the-footer',
+});
+</script>
+
+<style scoped>
+
+</style>
