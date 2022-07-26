0333d1ddde48712f7236e96b579bd9f74864ebc3
ZhiyuWang <zcabanq@uc.ac.uk>
2022-07-26 16:31:48 +0800

    add CopyUtil

--- src/main/java/com/example/springbootproject/service/EbookService.java
+++ src/main/java/com/example/springbootproject/service/EbookService.java
@@ -6,11 +6,10 @@ import com.example.springbootproject.domain.EbookExample;
 import com.example.springbootproject.mapper.EbookMapper;
 import com.example.springbootproject.req.EbookReq;
 import com.example.springbootproject.resp.EbookResp;
-import org.springframework.beans.BeanUtils;
+import com.example.springbootproject.utils.CopyUtil;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -23,13 +22,17 @@ public class EbookService {
         EbookExample.Criteria criteria = ebookExample.createCriteria();
         criteria.andNameLike("%" + req.getName() + "%");
         List<Ebook> ebookList = ebookMapper.selectByExample(ebookExample);
-        //put all the ebooks in an ebook response list
+        /*
+        put all the ebooks in an ebook response list
         List<EbookResp> respList = new ArrayList<>();
         for (Ebook ebook : ebookList) {
             EbookResp ebookResp = new EbookResp();
             BeanUtils.copyProperties(ebook, ebookResp);
             respList.add(ebookResp);
         }
+        */
+        List<EbookResp> respList = CopyUtil.copyList(ebookList, EbookResp.class);
+
         return respList;
     }
 }
