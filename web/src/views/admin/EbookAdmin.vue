4fc2313d7657b1cb8a8d04889f847bd56200b0ca
ZhiyuWang <zcabanq@uc.ac.uk>
2022-08-05 13:37:25 +0800

    修改电子书管理布局

--- web/src/views/admin/EbookAdmin.vue
+++ web/src/views/admin/EbookAdmin.vue
@@ -1,5 +1,9 @@
 <template>
-  <div class="admin-ebook">
-    ebook admin
-  </div>
+  <a-layout class="home">
+    <a-layout-content
+        :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
+    >
+      <h1>Ebook Management</h1>
+    </a-layout-content>
+  </a-layout>
 </template>
